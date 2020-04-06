1e0eea20612ce91d0fe78d885b6c6e4479b6379e
Alex Vladut <alexandruvladut@gmail.com>
2020-04-06 15:00:33 +0300

    feat(app): added graphql api infrastructure

---
+++ src/graphql/queries.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getTask = /* GraphQL */ `
+  query GetTask($id: ID!) {
+    getTask(id: $id) {
+      id
+      title
+      description
+      status
+      owner
+    }
+  }
+`;
+export const listTasks = /* GraphQL */ `
+  query ListTasks(
+    $filter: ModelTaskFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTasks(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        description
+        status
+        owner
+      }
+      nextToken
+    }
+  }
+`;

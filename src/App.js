715e5ad472b9a10f930e16da2feaefa5f3f75024
Alex Vladut <alexandruvladut@gmail.com>
2020-05-27 18:20:12 +0300

    chore(app): remove auth and api resources

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
 
-import { Auth } from '@aws-amplify/auth';
-
-import './App.css';
-
-function checkUser() {
-  Auth.currentAuthenticatedUser()
-    .then(user => console.log({ user }))
-    .catch(err => console.log(err))
-}
-
-function signOut() {
-  Auth.signOut()
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-}
-
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
-        <button onClick={checkUser}>Check User</button>
-        <button onClick={signOut}>Sign Out</button>
-      </header>
+    <div>
+     <nav>
+       <ul>
+         <li><a href="/home">Home</a></li>
+         <li><a href="/about">About</a></li>
+         <li><a href="signUp">Sign up</a></li>
+       </ul>
+     </nav>
     </div>
   );
 }
